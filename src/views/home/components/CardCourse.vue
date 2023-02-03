a3ca38fbe2cc9381e6a4bd032289a09e1e8bdcaf
Matheus-O-Silva <matheus.oliveira110101@gmail.com>
2023-02-03 15:06:19 -0300

    pagina de exibicao de player

--- src/views/home/components/CardCourse.vue
+++ src/views/home/components/CardCourse.vue
@@ -3,7 +3,10 @@
         <li>
             <span :class="['card', 'bg-' + bgName]">
                 <span class="icon">
-                    <img :src="[image != '' ? image : require('@/assets/images/icons/default.png')]" :alt="title" />
+                    <!-- <img :src="[image != '' ? image : require('@/assets/images/icons/default.png')]" :alt="title" /> -->
+                    <router-link :to="{name: 'campus.modulos'}" :class="['card', 'bg-' + bgName]">
+                        Acessar
+                    </router-link>
                 </span>
                 <span class="title">{{ title }}</span>
                 <span class="action">
---
+++ src/views/modules/ModulesAndLessons.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="pageTitle">
+            <span class="title">Meu Curso</span>
+            <span class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
+        </div>
+
+        <div class="content">
+            <div class="container">
+
+                <modules/>
+
+                <div class="right">
+                    <div class="content">
+
+                        <player/>
+
+                        <supports-lesson></supports-lesson>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Modules from './components/Modules.vue'
+import Player from './components/Player.vue'
+import SupportsLesson from './components/Supports.vue'
+
+export default {
+    name: 'ModulesAndLessons',
+    components: {
+        Modules,
+        Player,
+        SupportsLesson
+    }
+}
+
+</script>
